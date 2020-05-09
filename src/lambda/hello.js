c9e46337bb514e54f05ffef0697e3632c3edba0d
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-09 23:01:22 +0530

    updated gell[

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,69 +1,22 @@
-export const initZoom = async ({
-  meetingNumber = 79631636690,
-  userName = "default",
-}) => {
-  console.log("user", userName);
-  // lazy load script
-  const { ZoomMtg } = await import("@zoomus/websdk");
-  console.log(ZoomMtg);
-  if (typeof ZoomMtg !== "undefined") {
-    console.log("checkSystemRequirements");
-    console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
+import axios from "axios";
 
-    ZoomMtg.setZoomJSLib("https://source.zoom.us/1.7.7/lib", "/av"); // CDN version default
-    ZoomMtg.preLoadWasm();
-    ZoomMtg.prepareJssdk();
-    var API_KEY = "p_0XQpvIS6iBbOAVnre51g";
+// sample API URL
+// http://anuj-zoom.netlify.com/.netlify/functions/hello
 
-    /**
-     * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
-     * The below generateSignature should be done server side as not to expose your api secret in public
-     * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/web/essential/signature
-     */
-
-    var API_SECRET = "dKoWi6l1vwR1eg0hpGyeDwDjLEg6EWwFGPHg";
-
-    const meetConfig = {
-      apiKey: API_KEY,
-      apiSecret: API_SECRET,
-      meetingNumber,
-      userName,
-      passWord: "anuj",
-      leaveUrl: "https://anuj-zoom.netlify.app/zoom/",
-      role: 0,
-    };
-
-    ZoomMtg.generateSignature({
-      meetingNumber: meetConfig.meetingNumber,
-      apiKey: meetConfig.apiKey,
-      apiSecret: meetConfig.apiSecret,
-      role: meetConfig.role,
-      success(res) {
-        console.log("signature", res.result);
-
-        ZoomMtg.init({
-          leaveUrl: "https://anuj-zoom.netlify.app/zoom/",
-          success() {
-            ZoomMtg.join({
-              meetingNumber: meetConfig.meetingNumber,
-              userName: meetConfig.userName,
-              signature: res.result,
-              apiKey: meetConfig.apiKey,
-              passWord: meetConfig.passWord,
-              success() {
-                //  $("#nav-tool").hide();
-                console.log("join meeting success");
-              },
-              error(res) {
-                console.log(res);
-              },
-            });
-          },
-          error(res) {
-            console.log(res);
-          },
-        });
+exports.handler = (event, context, callback) => {
+  axios
+    .get("https://api.airtable.com/v0/appzhdjp7tk4G73Bh/Table%201", {
+      headers: {
+        Authorization: "Bearer keypXdhotixFUmd7K",
       },
+    })
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res.data),
+      });
+    })
+    .catch((err) => {
+      callback(err);
     });
-  }
 };
