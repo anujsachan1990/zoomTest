9ba759c41841fe4614968509ad825285c9aab05e
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-11 16:27:44 +0530

    test with iframe

--- src/components/IframeWithMeeting.js
+++ src/components/IframeWithMeeting.js
@@ -13,12 +13,14 @@ export default function IframeWithMeeting() {
         width="100%"
         height="500"
         allow="camera;microphone"
+        SameSite="Lax"
       />
       <iframe
         id="slido"
         src={"https://app.sli.do/event/qly6e4ur/live/polls?w=cOWOT"}
         width="100%"
         height="500"
+        SameSite="Lax"
       />
     </div>
   );
