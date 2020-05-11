a2228b893920f13dfecd590afc079152716784c8
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-11 22:28:32 +0530

    revert routing

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,14 +1,6 @@
 // import ...
 import React from "react";
-import { navigate } from "@reach/router";
-import { useAuth } from "react-use-auth";
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
-  const { isAuthenticated } = useAuth();
-
-  if (!isAuthenticated()) {
-    navigate("/");
-    return null;
-  }
   return <Component {...rest} />;
 };
 export default PrivateRoute;
