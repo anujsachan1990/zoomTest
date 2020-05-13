e304d8d23386ed9c652ba126d98dadb3ee47cbd0
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-13 13:11:52 +0530

    change videos

--- src/pages/video.js
+++ src/pages/video.js
@@ -6,8 +6,9 @@ const Video = () => {
   return (
     <Layout hideLogin>
       <h1>test with One Stream</h1>
+
       <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F557277038497489%2F"
+        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F336073037379749%2F"
         width="500"
         height="280"
         scrolling="no"
@@ -18,7 +19,7 @@ const Video = () => {
       ></iframe>
 
       <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F956062928177776%2F"
+        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F641959063024208%2F"
         width="500"
         height="280"
         scrolling="no"
@@ -29,7 +30,7 @@ const Video = () => {
       ></iframe>
 
       <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F378823583034783%2F"
+        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F3126622190730388%2F"
         width="500"
         height="280"
         scrolling="no"
@@ -40,7 +41,7 @@ const Video = () => {
       ></iframe>
 
       <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F2586421198354058%2F"
+        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F260515621970426%2F"
         width="500"
         height="280"
         scrolling="no"
