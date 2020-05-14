1752848295045509af4ae427c2e136a3a97260ad
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-14 09:03:30 +0530

    validate user added

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,23 @@
 // import ...
 import React from "react";
+import { navigate } from "@reach/router";
+import { useAuth } from "react-use-auth";
+
 import Layout from "../components/layout";
+
 const PrivateRoute = ({
   component: Component,
   location,
   hideLayout,
   ...rest
 }) => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated()) {
+    navigate("/");
+    return null;
+  }
+
   return hideLayout ? (
     <Component {...rest} />
   ) : (
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,50 +11,6 @@ const IndexPage = () => {
   const { isAuthenticated } = useAuth();
   return (
     <Layout hideLogin>
-      <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F557277038497489%2F"
-        width="500"
-        height="280"
-        scrolling="no"
-        frameborder="0"
-        allowTransparency="true"
-        allow="encrypted-media"
-        allowFullScreen="true"
-      ></iframe>
-
-      <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F956062928177776%2F"
-        width="500"
-        height="280"
-        scrolling="no"
-        frameborder="0"
-        allowTransparency="true"
-        allow="encrypted-media"
-        allowFullScreen="true"
-      ></iframe>
-
-      <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F378823583034783%2F"
-        width="500"
-        height="280"
-        scrolling="no"
-        frameborder="0"
-        allowTransparency="true"
-        allow="encrypted-media"
-        allowFullScreen="true"
-      ></iframe>
-
-      <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F2586421198354058%2F"
-        width="500"
-        height="280"
-        scrolling="no"
-        frameborder="0"
-        allowTransparency="true"
-        allow="encrypted-media"
-        allowFullScreen="true"
-      ></iframe>
-
       <SEO title="Home" />
       <Placeholder>
         {!isAuthenticated() ? (
