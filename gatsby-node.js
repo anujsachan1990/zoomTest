45e67db2912d6f5f3a389faddbf4c52b4ee11ff0
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-14 17:19:20 +0530

    test

--- gatsby-node.js
+++ gatsby-node.js
@@ -24,16 +24,3 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     });
   }
 };
-
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions;
-
-  // page.matchPath is a special key that's used for matching pages
-  // only on the client.
-  if (page.path.match(/^\/zoom/)) {
-    page.matchPath = "/zoom/*";
-
-    // Update the page.
-    createPage(page);
-  }
-};
