82b590b9c67320cb0b8052941c1f83dbde4dff6f
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-14 06:30:33 +0530

    added change for video

--- src/pages/video.js
+++ src/pages/video.js
@@ -8,9 +8,9 @@ const Video = () => {
       <h1>test with One Stream and youtube</h1>
 
       <iframe
-        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F268102620993722%2F&show_text=0&width=476"
-        width="476"
-        height="476"
+        src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FoojsRock%2Fvideos%2F1552934741524051%2F&show_text=0&width=560"
+        width="800"
+        height="900"
         scrolling="no"
         frameborder="0"
         allowTransparency="true"
