6ec33d9572d0a5524ba41743fba5bea860a62e4c
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-17 16:08:56 +0530

    updayed

--- src/functions/apitest.js
+++ src/functions/apitest.js
@@ -4,6 +4,29 @@ const { checkHeaderForValidToken } = require("./auth.js");
 // sample API URL
 // http://anuj-zoom.netlify.com/.netlify/functions/hello
 
+const data = () =>
+  axios
+    .get("https://api.airtable.com/v0/appzhdjp7tk4G73Bh/Table%201", {
+      headers: {
+        Authorization: "Bearer keypXdhotixFUmd7K",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+      },
+    })
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res.data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    })
+    .catch((err) => {
+      callback(err);
+    });
+
 exports.handler = async (event, context, callback) => {
   try {
     await checkHeaderForValidToken({
@@ -19,27 +42,6 @@ exports.handler = async (event, context, callback) => {
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({
-      statusCode: 200,
-    }),
+    body: data(),
   });
-
-  // axios
-  //   .get("https://api.airtable.com/v0/appzhdjp7tk4G73Bh/Table%201", {
-  //     headers: {
-  //       Authorization: "Bearer keypXdhotixFUmd7K",
-  //     },
-  //   })
-  //   .then((res) => {
-  //     callback(null, {
-  //       statusCode: 200,
-  //       body: JSON.stringify(res.data),
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     callback(err);
-  //   });
 };
