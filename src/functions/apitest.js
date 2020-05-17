0a64178ed9268d4763f8ab071197512cf5359721
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-17 16:20:27 +0530

    updated

--- src/functions/apitest.js
+++ src/functions/apitest.js
@@ -9,6 +9,28 @@ exports.handler = async (event, context, callback) => {
     await checkHeaderForValidToken({
       authorization: event.headers.authorization,
     });
+
+    axios
+      .get("https://api.airtable.com/v0/appzhdjp7tk4G73Bh/Table%201", {
+        headers: {
+          Authorization: "Bearer keypXdhotixFUmd7K",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers":
+            "Origin, X-Requested-With, Content-Type, Accept",
+        },
+      })
+      .then((res) => {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(res.data),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      })
+      .catch((err) => {
+        callback(err);
+      });
   } catch (err) {
     console.error(err);
     return {
@@ -16,26 +38,4 @@ exports.handler = async (event, context, callback) => {
       body: JSON.stringify({ msg: err }),
     };
   }
-
-  axios
-    .get("https://api.airtable.com/v0/appzhdjp7tk4G73Bh/Table%201", {
-      headers: {
-        Authorization: "Bearer keypXdhotixFUmd7K",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers":
-          "Origin, X-Requested-With, Content-Type, Accept",
-      },
-    })
-    .then((res) => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(res.data),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    })
-    .catch((err) => {
-      callback(err);
-    });
 };
