2d6885b7fcc6b26dc44eb2d7731510387d88a28a
ANUJ SACHAN <anuj.sachan@akqa.com>
2020-05-17 15:41:12 +0530

    added changes

--- src/pages/apimanagement.js
+++ src/pages/apimanagement.js
@@ -4,7 +4,12 @@ import Layout from "../components/layout";
 
 const makeAPICall = async () => {
   const data = await fetch(
-    "https://anuj-zoom.netlify.app/.netlify/functions/apitest"
+    "https://anuj-zoom.netlify.app/.netlify/functions/apitest",
+    {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }
   );
   console.log(data);
 };
